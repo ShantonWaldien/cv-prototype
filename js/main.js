e2c3f7149cff903b925e77e687026eb70772702e
ShantonWaldien <waldienshanton@gmail.com>
2018-12-05 12:29:16 +0200

    created navbar

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+/***** Navbar scroll color change *******/
+
+window.addEventListener('scroll', function (e) {
+    var nav = document.getElementById('nav');
+    if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
+            nav.classList.add('nav-colored');
+            nav.classList.add('nav-height');
+        } else {
+            nav.classList.remove('nav-colored');
+            nav.classList.remove('nav-height');
+        }
+});
