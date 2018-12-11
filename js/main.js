6c519ecb02c1f40744ba815de6e135e85689cbfc
ShantonWaldien <waldienshanton@gmail.com>
2018-12-11 08:41:13 +0200

    adjusted font sizes

--- js/main.js
+++ js/main.js
@@ -5,11 +5,9 @@ window.addEventListener('scroll', function (e) {
     var nav = document.getElementById('nav');
     if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
             nav.classList.add('nav-colored');
-            nav.classList.add('nav-height');
             nav.classList.add('nav-text');
         } else {
             nav.classList.remove('nav-colored');
-            nav.classList.remove('nav-height');
             nav.classList.remove('nav-text');
         }
 });
